3349412f173e0cbbae9cbcff425b977e6d4f3c2e
Ari Lerner <arilerner@mac.com>
2009-06-06 22:46:27 -0700

    Updated config test to add tests

--- test/bakers/config_test.rb
+++ test/bakers/config_test.rb
@@ -14,6 +14,29 @@ class ConfigTest < Test::Unit::TestCase
       c = Baker::Config.new
       assert_equal "cookbook_path     \"/etc/chef/cookbooks\"\nnode_path         \"/etc/chef/nodes\"\nlog_level         :info\nfile_store_path  \"/etc/chef\"\nfile_cache_path  \"/etc/chef\"\n", c.content
     end
+    
+    context "compiling" do
+      setup do
+        @cookbook_directory = "#{File.dirname(__FILE__)}/../test_dir"
+        @config = Baker::Config.new("config")
+        @config.cookbook_directory = @cookbook_directory
+        FileUtils.rm_rf @cookbook_directory if File.directory?(@cookbook_directory)
+      end
+
+      teardown do
+        FileUtils.rm_rf @cookbook_directory if File.directory?(@cookbook_directory)
+      end
+
+      should "create the template directory (since it doesn't exist)" do
+        assert !File.directory?("#{@cookbook_directory}/config")
+        @config.compile("burbary")
+        assert File.directory?("#{@cookbook_directory}/config")
+      end
+      should "store the content in the new file" do
+        @config.compile("burbary")
+        assert_equal "config", open("#{@cookbook_directory}/config/burbary").read
+      end
+    end
   end
   
 end
